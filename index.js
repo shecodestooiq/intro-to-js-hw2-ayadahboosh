893e08193563c14a1dd2cae150adc797aa61d463
ayaahmed <ayaahmeed963@gmail.com>
2024-02-04 23:09:22 +0300

    hw2

---
+++ index.js
@@ -0,0 +1,11 @@
+var number1=10
+var number2=5
+
+console.log("1 gearter than 2" ,number1> number2);
+console.log("1 less than 2" ,number1< number2);
+console.log("equals " ,number1== number2);
+console.log("greater or equals", number1>=number2);
+console.log("less or equals", number1<=number2)
+console.log("not" , !(number1< number2));
+console.log("or" ,number1> number2 || number2==17);
+console.log("and", number1>number2 && number2==5);
